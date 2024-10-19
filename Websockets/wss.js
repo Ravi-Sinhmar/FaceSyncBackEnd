a8a0a1965d283350a07099c9c22f048b73fe580a
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-19 19:33:01 +0530

    Websockete to Socket.io

--- Websockets/wss.js
+++ Websockets/wss.js
@@ -1,86 +1,60 @@
 // websocket server configration using same app on which express server is running...
 const { app } = require("./../app");
-const http = require("http");
+const http = require('http');
+const { Server } = require('socket.io');
 const server = http.createServer(app);
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
+const io = new Server(server, {
+  cors: {
+    origin: "https://live-face.vercel.app", // Replace with your React app URL
+    methods: ["GET", "POST"]
+  }
+});
+
 
 // database models or collections
 const meets = require("./../Models/meets");
 
 // custom controllers
 const { getCookies } = require("./../Controllers/getCookies");
-const {
-  formatString,
-  extractMeetingId,
-} = require("./../Controllers/Common/common");
+const {formatString,extractMeetingId} = require('./../Controllers/Common/common');
 
 // const cleanName =  userName.toLowerCase().replace(/\s+/g, "");
 
-// Global Varibales
-const allConnections = new Map(); //Array to Store Websockets of both the connection
-
-wss.on("connection", async (ws, req) => {
-  const url = req.url;
-  const parts = url.split("fullMeetId=")[1];
-  const meetingId = parts.split("__.")[0];
-  const type = parts.split("__.")[1];
-
-  try {
-    const meet = await meets.findOne({ meetingId: meetingId });
-    if (meet) {
-      allConnections.set(type, ws);
-      console.log("Connected",type);
-    
-    } else {
-      ws.close();
-    }
-  } catch (error) {
-    console.log("In Catch", error);
-    ws.close();
-  }
-  // If Message Comes
-  ws.on("message", async (message) => {
-    let msg = JSON.parse(message);
-    // let msg = message
-    console.log("got messg", msg);
-    let sender = null;
-    let receiver = null;
-    if (msg.admin) {
-      sender = "ad";
-      receiver = "us";
-    } else {
-      sender = "us";
-      receiver = "ad";
-    }
-    if (allConnections.has(sender) && allConnections.has(receiver)) {
-      const fws = allConnections.get(receiver);
-      if (fws.readyState === WebSocket.OPEN) {
-        fws.send(JSON.stringify(msg));
-      }
-    } else if (allConnections.has(sender)) {
-      const fws = allConnections.get(sender);
-      if (fws.readyState === WebSocket.OPEN) {
-        fws.send(JSON.stringify({ OnlyAvailable: sender }));
-      }
-    } else if (allConnections.has(receiver)) {
-      const fws = allConnections.get(receiver);
-      if (fws.readyState === WebSocket.OPEN) {
-        fws.send(JSON.stringify({ OnlyAvailable: receiver }));
-      }
-    }
+const allusers = {};
+// handle socket connections
+io.on("connection", (socket) => {
+  console.log(`Someone connected to socket server and socket id is ${socket.id}`);
+  socket.on("join-user", username => {
+      console.log(`${username} joined socket connection`);
+      allusers[username] = { username, id: socket.id };
+      // inform everyone that someone joined
+      io.emit("joined", allusers);
   });
 
-  // If Error Comes
-  ws.on("error", (err) => {
-    console.error(`Error from ws.on error ${err}`);
+  socket.on("offer", ({from, to, offer}) => {
+      console.log({from , to, offer });
+      io.to(allusers[to].id).emit("offer", {from, to, offer});
   });
 
-  // If WebSocket Closes
-  ws.on("close", async () => {
-    console.log("User Disconnected");
-    allConnections.delete(type);
+  socket.on("answer", ({from, to, answer}) => {
+     io.to(allusers[from].id).emit("answer", {from, to, answer});
   });
-});
+
+  socket.on("end-call", ({from, to}) => {
+      io.to(allusers[to].id).emit("end-call", {from, to});
+  });
+
+  socket.on("call-ended", caller => {
+      const [from, to] = caller;
+      io.to(allusers[from].id).emit("call-ended", caller);
+      io.to(allusers[to].id).emit("call-ended", caller);
+  })
+
+  socket.on("icecandidate", candidate => {
+      console.log({ candidate });
+      //broadcast to other peers
+      socket.broadcast.emit("icecandidate", candidate);
+  }); 
+})
 
 module.exports = server;
