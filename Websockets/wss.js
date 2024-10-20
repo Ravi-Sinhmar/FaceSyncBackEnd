fe9c9ede29991bc1a7786b3e879f0b7c7a654307
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-10-21 00:58:03 +0530

    added console.log to check

--- Websockets/wss.js
+++ Websockets/wss.js
@@ -27,6 +27,7 @@ io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
   socket.on("room:join", (data) => {
     const { email, room } = data;
+    console.log("Server (room:join):Email , room",email,room);
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
     io.to(room).emit("user:joined", { email, id: socket.id });
@@ -35,19 +36,25 @@ io.on("connection", (socket) => {
   });
 
   socket.on("user:call", ({ to, offer }) => {
+    console.log("Server (user:call):to , offer",to,offer);
     io.to(to).emit("incomming:call", { from: socket.id, offer });
   });
 
   socket.on("call:accepted", ({ to, ans }) => {
+    console.log("Server (call:accepted):to , ans",to,ans);
+
     io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
 
   socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("Server (peer:nego:needed):to , offer",to,offer);
+
     console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
   });
 
   socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("Server (peer:nego:done):to , ans",to,ans);
     console.log("peer:nego:done", ans);
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
